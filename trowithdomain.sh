51ebbaba65845f34eb421afb1c26f1116c47a680
SoundOfTwitter <140434271+SoundOfTwitter@users.noreply.github.com>
2025-11-14 22:00:26 +0800

    Update trowithdomain.sh

--- trowithdomain.sh
+++ trowithdomain.sh
@@ -22,10 +22,10 @@ systemctl enable nginx
 systemctl start nginx
 
 # 使用 certbot 为域名申请证书（自动配置 nginx）
-#certbot --nginx -d "$server_domain" --non-interactive --agree-tos --email admin@$server_domain || {
-#    echo "错误：证书申请失败！"
-#    exit 1
-#}
+certbot --nginx -d "$server_domain" --non-interactive --agree-tos --email admin@$server_domain || {
+    echo "错误：证书申请失败！"
+    exit 1
+}
 
 # 创建或覆盖续期任务（每天 0点 和 12点 执行两次）
 echo "0 0,12 * * * root certbot renew --quiet --nginx" > /etc/cron.d/certbot-renew
