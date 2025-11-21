f3b61443b26f56816bb2ce715c874be6255be98c
SoundOfTwitter <140434271+SoundOfTwitter@users.noreply.github.com>
2025-11-21 08:50:06 +0800

    Update trowithdomain.sh

--- trowithdomain.sh
+++ trowithdomain.sh
@@ -19,7 +19,7 @@ apt install -y curl jq nginx trojan
 apt install -y certbot python3-certbot-nginx
 
 systemctl enable nginx
-#systemctl start nginx
+systemctl start nginx
 
 # 使用 certbot 为域名申请证书（自动配置 nginx）
 certbot --nginx -d "$server_domain" --non-interactive --agree-tos --email admin@$server_domain || {
