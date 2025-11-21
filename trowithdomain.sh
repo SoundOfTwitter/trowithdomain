61fb1380b0196a4e32272840595e1aa615289a97
SoundOfTwitter <140434271+SoundOfTwitter@users.noreply.github.com>
2025-11-21 12:36:17 +0800

    Update trowithdomain.sh

--- trowithdomain.sh
+++ trowithdomain.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
 # 输入密码
-#read -sp "请输入密码 (passwd): " passwd
 read -p "请输入密码 (passwd): " passwd
-echo
-
 # 输入域名
 read -p "请输入域名 (server_domain): " server_domain
-
 # 检查输入
 if [ -z "$passwd" ] || [ -z "$server_domain" ]; then
     echo "错误：密码和域名不能为空！"
@@ -16,20 +12,8 @@ fi
 
 apt install -y curl jq nginx trojan
 
-# 安装 certbot 和 nginx 插件
-apt install -y certbot
-#apt install -y python3-certbot-nginx
-
 systemctl enable nginx
 systemctl start nginx
-
-# systemctl stop nginx
-certbot certonly --standalone -d "$server_domain" --email admin@$server_domain --agree-tos --noninteractive
-# systemctl start nginx
-
-
-# 创建或覆盖续期任务（每天 0点 和 12点 执行两次）
-# echo "0 0,12 * * * root certbot renew --quiet --nginx" > /etc/cron.d/certbot-renew
 
 CONFIG="/etc/trojan/config.json"
 # 修改 password
@@ -41,11 +25,7 @@ jq --arg d "$server_domain" '
   | .ssl.key = "/etc/letsencrypt/live/\($d)/privkey.pem"
   | .ssl.alpn += ["h2"]
 ' "$CONFIG" > /tmp/config.tmp && mv /tmp/config.tmp "$CONFIG"
-
 echo "配置已更新：$CONFIG"
 
 systemctl enable trojan
 systemctl start trojan
-
-systemctl stop nginx
-certbot certonly --standalone --deploy-hook "systemctl restart nginx" -d $server_domain --email admin@$server_domain --agree-tos --noninteractive
