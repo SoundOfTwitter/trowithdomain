d0905ac4ffa1b5ea2ef60df0dd1a9d8e4a2c8ff6
SoundOfTwitter <140434271+SoundOfTwitter@users.noreply.github.com>
2025-11-21 08:49:36 +0800

    Update trowithdomain.sh

--- trowithdomain.sh
+++ trowithdomain.sh
@@ -22,10 +22,10 @@ systemctl enable nginx
 #systemctl start nginx
 
 # 使用 certbot 为域名申请证书（自动配置 nginx）
-#certbot --nginx -d "$server_domain" --non-interactive --agree-tos --email admin@$server_domain || {
-#    echo "错误：证书申请失败！"
-#    exit 1
-#}
+certbot --nginx -d "$server_domain" --non-interactive --agree-tos --email admin@$server_domain || {
+    echo "错误：证书申请失败！"
+    exit 1
+}
 systemctl stop nginx
 # certbot certonly --standalone -d "$server_domain" --email admin@$server_domain --agree-tos --noninteractive
 systemctl start nginx
